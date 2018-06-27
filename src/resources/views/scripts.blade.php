459f3aec275102954839adde079381420ac7bbcc
Jeff Kilroy <jeffkilroy@gmail.com>
2018-06-27 03:54:13 -0400

    Call after onload to prevent issues with Vuejs

--- src/resources/views/scripts.blade.php
+++ src/resources/views/scripts.blade.php
@@ -1,25 +1,42 @@
 <script src="https://brainblocks.io/brainblocks.min.js"></script>
     <script>
-    	var brainBlocksElements = document.querySelectorAll("[rel='brainblocks-button']");
-    	if(brainBlocksElements.length > 0){
-    		console.log(brainBlocksElements);
-    		for (i = 0; i < brainBlocksElements.length; ++i) {
-    			var brainBlocksElement = brainBlocksElements[i];
-    			var brainBlocksButtonId = brainBlocksElement.dataset.id;
-    			var brainBlocksButtonAmount = brainBlocksElement.dataset.amount;
-                var brainBlocksButtonDestination = brainBlocksElement.dataset.destination;
-                var brainBlocksButtonCurrency = brainBlocksElement.dataset.currency;
-    			var brainBlocksRenderer = brainblocks.Button.render({
-		            payment: {
-		                destination: brainBlocksButtonDestination,
-		                currency: brainBlocksButtonCurrency,
-		                amount: brainBlocksButtonAmount
-		            },
-		            onPayment: function (data) {
-		                document.getElementById(brainBlocksButtonId+"-token").value = data.token;
-		                document.getElementById(brainBlocksButtonId+"-form").submit();
-		            }
-		        }, '#'+brainBlocksButtonId+'-button');
-    		}
-    	}       
+        if(window.attachEvent) {
+            window.attachEvent('onload', processBrainBlocks);
+        } else {
+            if(window.onload) {
+                var curronload = window.onload;
+                var newonload = function(evt) {
+                    curronload(evt);
+                    processBrainBlocks(evt);
+                };
+                window.onload = newonload;
+            } else {
+                window.onload = processBrainBlocks;
+            }
+        }
+
+        function processBrainBlocks() {
+            var brainBlocksElements = document.querySelectorAll("[rel='brainblocks-button']");
+            if (brainBlocksElements.length > 0) {
+                console.log(brainBlocksElements);
+                for (i = 0; i < brainBlocksElements.length; ++i) {
+                    var brainBlocksElement = brainBlocksElements[i];
+                    var brainBlocksButtonId = brainBlocksElement.dataset.id;
+                    var brainBlocksButtonAmount = brainBlocksElement.dataset.amount;
+                    var brainBlocksButtonDestination = brainBlocksElement.dataset.destination;
+                    var brainBlocksButtonCurrency = brainBlocksElement.dataset.currency;
+                    var brainBlocksRenderer = brainblocks.Button.render({
+                        payment: {
+                            destination: brainBlocksButtonDestination,
+                            currency: brainBlocksButtonCurrency,
+                            amount: brainBlocksButtonAmount
+                        },
+                        onPayment: function (data) {
+                            document.getElementById(brainBlocksButtonId + "-token").value = data.token;
+                            document.getElementById(brainBlocksButtonId + "-form").submit();
+                        }
+                    }, '#' + brainBlocksButtonId + '-button');
+                }
+            }
+        }
     </script>
