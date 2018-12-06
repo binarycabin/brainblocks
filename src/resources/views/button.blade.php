f29e1140f4aa84ffca773548c2a49f232197226d
Jeff Kilroy <jeffkilroy@gmail.com>
2018-12-06 17:20:15 -0500

    Update button.blade.php
    Ability to pick form posting method

--- src/resources/views/button.blade.php
+++ src/resources/views/button.blade.php
@@ -12,9 +12,15 @@ if(empty($expanded)){
 if(empty($size)){
 	$size = 'responsive';
 }
+if(empty($method)){
+	$method = 'POST';
+}
 ?>
 <div v-pre rel="brainblocks-button" data-id="{{ $buttonId }}" data-amount="{{ $amount }}" data-destination="{{ $destination }}" data-currency="{{ $currency }}" data-size="{{ $size }}" data-expanded="{{ $expanded }}" id="{{ $buttonId }}-button"></div>
-<form action="{{ $action }}" method="POST" id="{{ $buttonId }}-form">
+<form action="{{ $action }}" @if($method == 'GET') method="GET" @else method="POST" @endif id="{{ $buttonId }}-form">
+    @if(!in_array($method,['GET','POST']))
+    <input type="hidden" name="_method" value="{{ $method }}" />
+    @endif
     <input type="hidden" name="brainblocks_token" id="{{ $buttonId }}-token" value="" />
     {{ csrf_field() }}
 </form>
